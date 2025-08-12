1b6cfc02a1dc0972620afb4a07f54b14d88b4b5c
Mamindlapelli Rajeev Hrushikesh <hrushikeshm56@gmail.com>
2025-08-12 09:30:25 +0530

    Time: 3 ms (45.71%), Space: 11 MB (5.36%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = "";
+        s = to_string(x);
+        int n = s.size();
+
+        int i = 0 , j = n-1;
+        while(i<=j)
+        {
+            if(s[i]!=s[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+        
+    }
+};
