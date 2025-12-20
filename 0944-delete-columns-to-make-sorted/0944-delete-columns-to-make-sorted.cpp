faabbd6677ce7386caefb559284974d27359e65f
Mamindlapelli Rajeev Hrushikesh <hrushikeshm56@gmail.com>
2025-12-20 09:46:09 +0530

    Time: 7 ms (20.29%), Space: 15.4 MB (77.57%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int cnt = 0 ; 
+    
+        int n = strs.size();
+        for(int i = 0  ; i < strs[0].size() ; i++)
+        {
+            for(int j = 0 ; j < n ; j++)
+            {
+                if(j+1<n)
+                {
+                    if(strs[j][i]>strs[j+1][i]) {
+                        cnt++;
+                        break;
+                }
+            }
+            }
+            }
+        return cnt;
+    }
+};
