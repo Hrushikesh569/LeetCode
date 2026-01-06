b61028454fa82df376ec997229d577d57e46d889
Mamindlapelli Rajeev Hrushikesh <hrushikeshm56@gmail.com>
2026-01-06 08:07:58 +0530

    Time: 4 ms (57.77%), Space: 108.3 MB (89.84%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -11,30 +11,48 @@
  */
 class Solution {
 public:
+    map<int , int>mp;
+  void dfs(TreeNode* root , int level)
+    {
+        if(!root) return;
+        mp[level]+=root->val;
+        dfs(root->left,level+1);
+        dfs(root->right,level+1);
+    }
     int maxLevelSum(TreeNode* root) {
         queue<TreeNode*>q;
         q.push(root);
         int maxSum = -1e9+7;  
         int level = 0 ; 
-        int curLevel = 1 ; 
-        while(!q.empty())
+        // int curLevel = 1 ; 
+        // while(!q.empty())
+        // {
+        //     int n = q.size();
+        //     int sum = 0 ; 
+        //     while(n--)
+        //     {
+        //         TreeNode* temp = q.front();
+        //         q.pop();
+        //         sum+=temp->val;
+        //         if(temp->left) q.push(temp->left);
+        //         if(temp->right)q.push(temp->right);
+        //     }
+        //     if(sum>maxSum)
+        //     {
+        //         maxSum = sum;
+        //         level = curLevel;
+        //     }
+        //     curLevel++;
+        // }
+        mp.clear();
+        dfs(root , 1);
+        for(auto [i,j]:mp)
         {
-            int n = q.size();
-            int sum = 0 ; 
-            while(n--)
+            if(maxSum<j)
             {
-                TreeNode* temp = q.front();
-                q.pop();
-                sum+=temp->val;
-                if(temp->left) q.push(temp->left);
-                if(temp->right)q.push(temp->right);
+                maxSum = j;
+                level = i;
             }
-            if(sum>maxSum)
-            {
-                maxSum = sum;
-                level = curLevel;
-            }
-            curLevel++;
         }
         return level;
     }
