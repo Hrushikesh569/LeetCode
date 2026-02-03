78bc05cfbdb91a0f159d764a345adfc15919b4cd
Mamindlapelli Rajeev Hrushikesh <hrushikeshm56@gmail.com>
2026-02-03 08:33:02 +0530

    Time: 2 ms (14.77%), Space: 27.7 MB (69.85%) - LeetHub

---
+++ 3637-trionic-array-i/3637-trionic-array-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size();
+        if(n==3)
+        {
+            return 0;
+        }
+        int i = 0 , j = n -1 ; 
+        for(; i< n - 1; i++)
+        {
+            if(nums[i]>=nums[i+1]) break;
+        }
+        for(;j>=1 ;j--)
+        {
+            if(nums[j-1]>=nums[j])
+            break;
+        }
+        cout<<i<<" "<<j<<endl;
+        if(i>=j || i==0 || j==n-1) return 0;
+        for(int st = i ; st < j ; st++)
+        {
+            if(nums[st]<=nums[st+1]) return 0 ;
+        }
+        return 1;
+    }
+};
