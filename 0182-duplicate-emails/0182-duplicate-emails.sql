1b11b5c6938db12ae915dd253d9ea29b659ed218
Mamindlapelli Rajeev Hrushikesh <hrushikeshm56@gmail.com>
2026-05-12 16:41:07 +0530

    Time: 422 ms (16.91%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,4 +1,2 @@
 # Write your MySQL query statement below
-
-
-select email as Email from Person group by email having count(email)>1;
+select distinct email from (select email,row_number() over (partition by email order by email) as dp from Person ) temp where dp>1
