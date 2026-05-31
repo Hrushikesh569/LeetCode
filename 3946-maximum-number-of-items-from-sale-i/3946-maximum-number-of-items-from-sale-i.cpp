25d8f4a2c17c5acad0fe865cc7d1a9c3ff75fdf6
Mamindlapelli Rajeev Hrushikesh <hrushikeshm56@gmail.com>
2026-05-31 14:22:53 +0530

    Time: 275 ms (17.85%), Space: 138.2 MB (11.2%) - LeetHub

--- 3946-maximum-number-of-items-from-sale-i/3946-maximum-number-of-items-from-sale-i.cpp
+++ 3946-maximum-number-of-items-from-sale-i/3946-maximum-number-of-items-from-sale-i.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    int dp[1000][1501];
-    vector<int> free;
-    int mini, n;
-    
-    int solve(int i, int tot, vector<vector<int>>& items) {
-        if(i>=items.size()) return tot/mini;
-        if(dp[i][tot]!=-1) return dp[i][tot];
-
-        int take=0;
-        if(tot>=items[i][1]) {
-            take=free[i]+solve(i+1, tot-items[i][1], items);
+    vector<vector<int>>dp;
+    int mini = 1e9;
+    int dfs(vector<vector<int>>&nums , vector<int>&v , int i  , int budget)
+    {
+        if(i>=nums.size()) return budget/mini ; 
+        if(dp[budget][i]!=-1) return dp[budget][i];
+        int not_take = dfs(nums,v ,  i+1 , budget);
+        int take  = 0 ; 
+        if(budget >= nums[i][1])
+        {
+            
+            take = dfs(nums , v , i +1, budget-nums[i][1])+v[i];
         }
-
-        int skip=solve(i+1, tot, items);
-        return dp[i][tot]=max(take, skip);
+        return dp[budget][i] = max(take , not_take);
     }
-    
     int maximumSaleItems(vector<vector<int>>& items, int budget) {
-        mini=INT_MAX, n=items.size();
-        free.resize(n, 0);
-
-        for(int i=0; i<n; i++) {
-            mini=min(mini, items[i][1]);
-            for(int j=0; j<n; j++) {
-                if(items[j][0]%items[i][0] == 0) free[i]++;
+        vector<int>v;
+        mini= 1e9;
+        int n = items.size();
+        for(int i = 0 ; i < n ;i++)
+        {
+            int cnt = 0 ; 
+            for(int j = 0 ; j < n ; j++)
+            {
+                mini = min(mini  , items[i][1]);
+                cnt+=(items[j][0]%items[i][0]==0);
             }
+            v.push_back(cnt);
         }
-
-        memset(dp, -1, sizeof(dp));
-        return solve(0, budget, items);
+        dp.clear();
+        dp = vector<vector<int>>(budget+1, vector<int>(n+1, -1));
+       return dfs(items , v , 0 , budget);
+        return 0;
     }
 };
