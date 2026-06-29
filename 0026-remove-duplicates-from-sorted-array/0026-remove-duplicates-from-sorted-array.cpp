6fb1fb0c233b007ae923dfcb0de4f47ed2385111
Mamindlapelli Rajeev Hrushikesh <hrushikeshm56@gmail.com>
2026-06-29 11:50:12 +0530

    Time: 0 ms (100%), Space: 22.7 MB (19.26%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,9 +4,9 @@ public:
         if(nums.size()==1) return 1;
         int st = 1 ; 
         int n = nums.size();
-        while(st<n and nums[st-1]!=nums[st]) st++;
+       // while(st<n and nums[st-1]!=nums[st]) st++;
         if(st==n) return st;
-        for(int i = st+1 ; i < n; i++)
+        for(int i = st ; i < n; i++)
         {
             if(nums[i]==nums[i-1]) continue;
             nums[st] = nums[i];
