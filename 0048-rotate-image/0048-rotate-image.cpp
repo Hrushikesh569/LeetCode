fcbda7c3528ed322a62d2b0bc2ce60bbe3eb57f2
Mamindlapelli Rajeev Hrushikesh <hrushikeshm56@gmail.com>
2026-06-29 16:57:59 +0530

    Time: 0 ms (100%), Space: 10.1 MB (68.32%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& nums) {
+        int n = nums.size();
+        for(int i = 0 ;i < n; i++)
+        {
+            for(int j = 0 ; j < i  ; j++)
+            {
+                swap(nums[i][j],nums[j][i]);
+            }
+        }
+        for(int i= 0 ; i < n ; i++)
+        {
+            reverse(nums[i].begin(),nums[i].end());
+        }
+        //return nums;
+    }
+};
