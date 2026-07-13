537030caf390b9a77ec5f46302397494e090579d
Mamindlapelli Rajeev Hrushikesh <hrushikeshm56@gmail.com>
2026-07-13 12:32:48 +0530

    Time: 0 ms (100%), Space: 12.7 MB (84.04%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -15,23 +15,24 @@ public:
     // }
     int minimumTotal(vector<vector<int>>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n+1 , vector<int>(n+2));
+        vector<int>cur(n),prev(n);
         for(int i = n - 1;  i >= 0 ; i--)
         {
-            dp[n-1][i] = nums[n-1][i];
+            prev[i] = nums[n-1][i];
         }
         for(int i = n - 2 ; i>= 0 ; i--)
         {
             for(int j = i ; j>=0 ; j--)
             {
-                dp[i][j] = min(dp[i+1][j+1],dp[i+1][j])+nums[i][j];
+                cur[j] = min(prev[j+1],prev[j])+nums[i][j];
             }
+            prev = cur;
         }
         // for(auto i : dp)
         // {
         //     for(auto j: i)cout<<j<<" ";
         //     cout<<endl;
         // }
-        return dp[0][0];
+        return prev[0];
     }
 };
